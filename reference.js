0dd90b3954e4cd92f56eaa07507034ac57f600e2
chucyfunny <125993050+chucyfunny@users.noreply.github.com>
2024-08-13 15:09:44 +0800

    Update reference.js

--- reference.js
+++ reference.js
@@ -1,9 +1,14 @@
+// 解析响应体
 let body = $response.body;
 let obj = JSON.parse(body);
+
+// 检查并提取'reference'字段
 if (obj.reference) {
-let reference = obj.reference;
-        // 发送通知
+    let reference = obj.reference;
+
+    // 发送通知
     $notify("Reference Extracted", "Reference: ", reference);
 }
 
-$done({});
+// 放行流量
+$done({body});
